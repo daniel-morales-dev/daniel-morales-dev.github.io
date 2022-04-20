c0911f8320e0fae1baf851a69c0c651c5586e888
Daniel Morales <mralejo99@gmail.com>
2022-04-20 13:53:43 -0500

    feat: removed old projects, change description, update contact view

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     contentBase: path.join(__dirname, "dist"),
     compress: true,
     port: 3000,
-    host: "192.168.0.12",
+    host: "localhost",
   },
   module: {
     rules: [
